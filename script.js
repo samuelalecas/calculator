9162c4bf847f1057bc310b27c95fc662afa693f5
Samuel Alegre <samuelalecas@gmail.com>
2022-09-13 13:21:55 +0200

    Set calculator to work properly Erase button and round results will be implemented next.

--- script.js
+++ script.js
@@ -7,13 +7,25 @@ function divide(a, b) { return a / b; }
 
 function operate(a, b, operation) { return operation(a, b); }
 
-let valueA = "";
-let operator = ""
-let valueB = 3;
-let screenValue;
+function operationSelector(operatorPressed) {
+    switch (operatorPressed) {
+        case "+":
+            return add;
+        case "-":
+            return subtract;
+        case "*":
+            return multiply;
+        case "/":
+            return divide;
+    }
+}
 
-let result = operate(operate(valueA, valueB, add), valueB, add)
-console.log(result);
+let lastValue = "";
+let operator = ""
+let currentValue = "";
+let screenValue = "";
+let numberIsTheResult = false;
+
 
 //? DOM Implementation
 function refresh() {
@@ -28,14 +40,38 @@ function createButton(id, type, character) {
     numberBtn.className = "button";
     if (type === "number") {
         numberBtn.addEventListener("click", () => {
-            valueA += `${character}`;
-            screenValue = valueA;
+            if (numberIsTheResult) {
+                currentValue = "";
+                screenValue = currentValue;
+                numberIsTheResult = false;
+                refresh()
+            }
+            currentValue += `${character}`;
+            screenValue += `${character}`;
             refresh();
         });
+    } else if (type === "operator") {
+        numberBtn.addEventListener("click", () => {
+            if (numberIsTheResult) numberIsTheResult = false;
+            operator = character;
+            lastValue = currentValue;
+            currentValue = "";
+            screenValue += ` ${character} `;
+            refresh();
+        })
+    } else if (type === "result") {
+        numberBtn.addEventListener("click", () => {
+            let currentOperation = operationSelector(operator);
+            lastValue = parseFloat(lastValue);
+            currentValue = parseFloat(currentValue);
+            currentValue = operate(lastValue, currentValue, currentOperation);
+            screenValue = currentValue;
+            numberIsTheResult = true;
+            refresh();
+        })
     }
     numbersButtons.append(numberBtn);
 }
-
 
 //? Calculator constructor
 for (let i = 1; i < 10; i++) {
@@ -43,7 +79,7 @@ for (let i = 1; i < 10; i++) {
 }
 createButton("numbers", "number", ".");
 createButton("numbers", "number", "0");
-createButton("numbers", "number", "=");
+createButton("numbers", "result", "=");
 
 const operators = ["+", "-", "*", "/"];
-operators.map((operator) => createButton("operators", "number", operator));
+operators.map((operator) => createButton("operators", "operator", operator));
